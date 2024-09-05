4b752c9b5ac581355b905b2857cde1e076ecf089
YousyCamila <166034381+YousyCamila@users.noreply.github.com>
2024-09-05 18:13:47 -0500

    feat(back-end): Add factura to layer BLL

---
+++ 16.PTCNCapas/PrivateInvestigationTecnology/BLL/FacturaExceptions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BLL
+{
+    public class FacturaExceptions : Exception
+    {
+        private FacturaExceptions(string message) : base(message) { }
+
+        public static void ThrowFacturaAlreadyExistsException(string descripcion, DateTime fechaEmision)
+        {
+            throw new FacturaExceptions($"Ya existe una factura con la descripción: {descripcion} y fecha de emisión: {fechaEmision}.");
+        }
+
+        public static void ThrowInvalidFacturaIdException(int id)
+        {
+            throw new FacturaExceptions($"La factura con ID {id} no existe.");
+        }
+
+        public static void ThrowInvalidFacturaDataException(string message)
+        {
+            throw new FacturaExceptions(message);
+        }
+    }
+}
+
