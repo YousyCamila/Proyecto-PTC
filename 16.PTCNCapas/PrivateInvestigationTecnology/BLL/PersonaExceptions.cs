0ee9de5209fc751f51c085cca11a6527d7a14614
YousyCamila <166034381+YousyCamila@users.noreply.github.com>
2024-09-05 18:21:34 -0500

    feat(back-end): Add persona to layer BLL

---
+++ 16.PTCNCapas/PrivateInvestigationTecnology/BLL/PersonaExceptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class PersonaExceptions : Exception
+    {
+        private PersonaExceptions(string message) : base(message) { }
+
+        public static void ThrowPersonaAlreadyExistsException(string dni)
+        {
+            throw new PersonaExceptions($"Ya existe una persona con el DNI: {dni}.");
+        }
+
+        public static void ThrowInvalidPersonaIdException(int id)
+        {
+            throw new PersonaExceptions($"La persona con ID {id} no existe.");
+        }
+
+        public static void ThrowInvalidPersonaDataException(string message)
+        {
+            throw new PersonaExceptions(message);
+        }
+    }
+}
