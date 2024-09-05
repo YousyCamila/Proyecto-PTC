b58b0903d79e0167d9420a1a8edf1e70236c2d7f
YousyCamila <166034381+YousyCamila@users.noreply.github.com>
2024-09-05 17:53:11 -0500

    feat(back-end): Add auditoria to layer BLL

---
+++ 16.PTCNCapas/PrivateInvestigationTecnology/BLL/AuditoriaExceptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace BLL
+{
+    public class AuditoriaExceptions : Exception
+    {
+        private AuditoriaExceptions(string message) : base(message) { }
+
+        public static void ThrowInvalidAuditoriaIdException(int id)
+        {
+            throw new AuditoriaExceptions($"Auditoría con ID {id} no existe.");
+        }
+
+        public static void ThrowInvalidAuditoriaDataException(string message)
+        {
+            throw new AuditoriaExceptions(message);
+        }
+    }
+}
