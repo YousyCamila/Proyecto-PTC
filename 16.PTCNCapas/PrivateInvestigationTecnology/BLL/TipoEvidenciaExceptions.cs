9712f594fb1fdcdd112b9cdc4c9c23fb2fae81d6
YousyCamila <166034381+YousyCamila@users.noreply.github.com>
2024-09-05 18:37:11 -0500

    feat(back-end): Add tipoEvidencia to layer BLL

---
+++ 16.PTCNCapas/PrivateInvestigationTecnology/BLL/TipoEvidenciaExceptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BLL
+{
+    public class TipoEvidenciaExceptions : Exception
+    {
+        private TipoEvidenciaExceptions(string message) : base(message) { }
+
+        public static void ThrowTipoEvidenciaAlreadyExistsException(string tipoDocumento)
+        {
+            throw new TipoEvidenciaExceptions($"Ya existe un tipo de evidencia con el documento: {tipoDocumento}.");
+        }
+
+        public static void ThrowInvalidTipoEvidenciaIdException(int id)
+        {
+            throw new TipoEvidenciaExceptions($"El tipo de evidencia con ID {id} no existe.");
+        }
+
+        public static void ThrowInvalidTipoEvidenciaDataException(string message)
+        {
+            throw new TipoEvidenciaExceptions(message);
+        }
+    }
+}
