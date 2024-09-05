332755e31be0966c613e686e71e93c8eaf8fa2a5
YousyCamila <166034381+YousyCamila@users.noreply.github.com>
2024-09-05 17:56:04 -0500

    feat(back-end): Add caso to layer BLL

---
+++ 16.PTCNCapas/PrivateInvestigationTecnology/BLL/CasoExceptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BLL
+{
+    public class CasosExceptions : Exception
+    {
+        private CasosExceptions(string message) : base(message) { }
+
+        public static void ThrowInvalidCasosIdException(int id)
+        {
+            throw new CasosExceptions($"Caso con ID {id} no existe.");
+        }
+
+        public static void ThrowInvalidCasosDataException(string message)
+        {
+            throw new CasosExceptions(message);
+        }
+    }
+}
