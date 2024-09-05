38052a36fcc25c6cd382d51448133a0c6c8dd351
YousyCamila <166034381+YousyCamila@users.noreply.github.com>
2024-09-05 18:01:19 -0500

    feat(back-end): Add cliente to layer BLL

---
+++ 16.PTCNCapas/PrivateInvestigationTecnology/BLL/ClienteExceptions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace BLL
+{
+    public class ClienteExceptions : Exception
+    {
+        private ClienteExceptions(string message) : base(message) { }
+
+        public static void ThrowClienteAlreadyExistsException(string direccion)
+        {
+            throw new ClienteExceptions($"Ya existe un cliente con la dirección: {direccion}.");
+        }
+
+        public static void ThrowInvalidClienteIdException(int id)
+        {
+            throw new ClienteExceptions($"El cliente con ID {id} no existe.");
+        }
+
+        public static void ThrowInvalidClienteDataException(string message)
+        {
+            throw new ClienteExceptions(message);
+        }
+    }
+}
+
