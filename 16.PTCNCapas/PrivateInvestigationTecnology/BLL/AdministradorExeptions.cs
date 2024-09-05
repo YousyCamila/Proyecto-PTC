7051dc17a75c8d7d5aa05e3006b6fe08d9fb2d2e
YousyCamila <166034381+YousyCamila@users.noreply.github.com>
2024-09-05 17:48:24 -0500

    feat(back-end): Add administrador to layer BLL

---
+++ 16.PTCNCapas/PrivateInvestigationTecnology/BLL/AdministradorExeptions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class AdministratorExceptions : Exception
+    {
+        private AdministratorExceptions(string message) : base(message)
+        {
+        }
+
+        public static void ThrowAdministratorAlreadyExistsException(string especialidad)
+        {
+            throw new AdministratorExceptions($"Un administrador con la especialidad '{especialidad}' ya existe.");
+        }
+
+        public static void ThrowInvalidAdministratorIdException(int id)
+        {
+            throw new AdministratorExceptions($"Administrador con ID {id} no existe.");
+        }
+
+        public static void ThrowInvalidAdministratorDataException(string message)
+        {
+            throw new AdministratorExceptions(message);
+        }
+    }
+}
