a816be47a49a1394cbd91bab044be204e03b9eea
YousyCamila <166034381+YousyCamila@users.noreply.github.com>
2024-09-08 13:51:53 -0500

    feat(backend): Add controller Factura

---
+++ 17.PTCMVC/PrivateInvestigationTechnology/Controllers/FacturasController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PrivateInvestigationTechnology.Datos;
+using PrivateInvestigationTechnology.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace PrivateInvestigationTechnology.Controllers
+{
+    [Authorize(Roles = "Administrador")] // Solo el Administrador puede acceder a todas las acciones
+    public class FacturasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FacturasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Facturas
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Facturas.Include(f => f.IdClienteNavigation);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Facturas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Facturas == null)
+            {
+                return NotFound();
+            }
+
+            var factura = await _context.Facturas
+                .Include(f => f.IdClienteNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (factura == null)
+            {
+                return NotFound();
+            }
+
+            return View(factura);
+        }
+
+        // GET: Facturas/Create
+        public IActionResult Create()
+        {
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "Id", "Id");
+            return View();
+        }
+
+        // POST: Facturas/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FechaEmision,EstadoPago,DescripcionServicio,TotalPagar,IdCliente")] Factura factura)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(factura);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "Id", "Id", factura.IdCliente);
+            return View(factura);
+        }
+
+        // GET: Facturas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Facturas == null)
+            {
+                return NotFound();
+            }
+
+            var factura = await _context.Facturas.FindAsync(id);
+            if (factura == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "Id", "Id", factura.IdCliente);
+            return View(factura);
+        }
+
+        // POST: Facturas/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FechaEmision,EstadoPago,DescripcionServicio,TotalPagar,IdCliente")] Factura factura)
+        {
+            if (id != factura.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(factura);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FacturaExists(factura.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "Id", "Id", factura.IdCliente);
+            return View(factura);
+        }
+
+        // GET: Facturas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Facturas == null)
+            {
+                return NotFound();
+            }
+
+            var factura = await _context.Facturas
+                .Include(f => f.IdClienteNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (factura == null)
+            {
+                return NotFound();
+            }
+
+            return View(factura);
+        }
+
+        // POST: Facturas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Facturas == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Facturas' is null.");
+            }
+            var factura = await _context.Facturas.FindAsync(id);
+            if (factura != null)
+            {
+                _context.Facturas.Remove(factura);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FacturaExists(int id)
+        {
+            return (_context.Facturas?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
