45c77206b5c5387f87e3e6c47c9ca191f6b58246
YousyCamila <166034381+YousyCamila@users.noreply.github.com>
2024-09-08 14:17:14 -0500

    feat(backend): Add controller RegistroMantenimiento

--- 17.PTCMVC/PrivateInvestigationTechnology/Models/Historial.cs
+++ 17.PTCMVC/PrivateInvestigationTechnology/Models/Historial.cs
@@ -4,37 +4,26 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
-namespace PrivateInvestigationTechnology.Models
+namespace PrivateInvestigationTechnology.Models;
+
+[Table("Historial")]
+[Index("IdCliente", Name = "IX_HISTORIAL_ID_Cliente")]
+public partial class Historial
 {
-    [Table("Historial")]
-    [Index("IdCliente", Name = "IX_HISTORIAL_ID_Cliente")]
-    public partial class Historial
-    {
-        [Key]
-        public int Id { get; set; }
+    [Key]
+    public int Id { get; set; }
 
-        public string Descripcion { get; set; } = null!;
+    public string Descripcion { get; set; } = null!;
 
-        [Column(TypeName = "datetime")]
-        public DateTime FechaInicio { get; set; }
+    [Column(TypeName = "datetime")]
+    public DateTime FechaInicio { get; set; }
 
-        [Column(TypeName = "datetime")]
-        public DateTime? FechaFinal { get; set; }
+    [Column(TypeName = "datetime")]
+    public DateTime? FechaFinal { get; set; }
 
-        public int IdCliente { get; set; }
-        public int IdCaso { get; set; }
-        public int IdDetective { get; set; }
+    public int IdCliente { get; set; }
 
-        [ForeignKey("IdCliente")]
-        [InverseProperty("Historials")]
-        public virtual Cliente IdClienteNavigation { get; set; } = null!;
-
-        [ForeignKey("IdCaso")]
-        [InverseProperty("Historials")]
-        public virtual Caso IdCasoNavigation { get; set; } = null!;
-
-        [ForeignKey("IdDetective")]
-        [InverseProperty("Historials")]
-        public virtual Detective IdDetectiveNavigation { get; set; } = null!;
-    }
+    [ForeignKey("IdCliente")]
+    [InverseProperty("Historials")]
+    public virtual Cliente IdClienteNavigation { get; set; } = null!;
 }
