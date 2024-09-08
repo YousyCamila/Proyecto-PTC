b71d84c860d29dffaffa919724d5fc0279ca7b4d
YousyCamila <166034381+YousyCamila@users.noreply.github.com>
2024-09-08 13:55:28 -0500

    feat(backend): Add controller Formulario

---
+++ 17.PTCMVC/PrivateInvestigationTechnology/Controllers/FormularioController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PrivateInvestigationTechnology.Datos;
+using PrivateInvestigationTechnology.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace PrivateInvestigationTechnology.Controllers
+{
+    [Authorize(Roles = "Administrador,Detective,Cliente")] // Ajustar roles según necesidad
+    public class FormulariosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FormulariosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Formularios
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Formularios.Include(f => f.IdClienteNavigation);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Formularios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Formularios == null)
+            {
+                return NotFound();
+            }
+
+            var formulario = await _context.Formularios
+                .Include(f => f.IdClienteNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (formulario == null)
+            {
+                return NotFound();
+            }
+
+            return View(formulario);
+        }
+
+        // GET: Formularios/Create
+        public IActionResult Create()
+        {
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "Id", "Nombre"); // Ajustar según la propiedad que deseas mostrar
+            return View();
+        }
+
+        // POST: Formularios/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Descripcion,IdCliente")] Formulario formulario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(formulario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "Id", "Nombre", formulario.IdCliente); // Ajustar según la propiedad que deseas mostrar
+            return View(formulario);
+        }
+
+        // GET: Formularios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Formularios == null)
+            {
+                return NotFound();
+            }
+
+            var formulario = await _context.Formularios.FindAsync(id);
+            if (formulario == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "Id", "Nombre", formulario.IdCliente); // Ajustar según la propiedad que deseas mostrar
+            return View(formulario);
+        }
+
+        // POST: Formularios/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Descripcion,IdCliente")] Formulario formulario)
+        {
+            if (id != formulario.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(formulario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FormularioExists(formulario.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "Id", "Nombre", formulario.IdCliente); // Ajustar según la propiedad que deseas mostrar
+            return View(formulario);
+        }
+
+        // GET: Formularios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Formularios == null)
+            {
+                return NotFound();
+            }
+
+            var formulario = await _context.Formularios
+                .Include(f => f.IdClienteNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (formulario == null)
+            {
+                return NotFound();
+            }
+
+            return View(formulario);
+        }
+
+        // POST: Formularios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Formularios == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Formularios' is null.");
+            }
+            var formulario = await _context.Formularios.FindAsync(id);
+            if (formulario != null)
+            {
+                _context.Formularios.Remove(formulario);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FormularioExists(int id)
+        {
+            return (_context.Formularios?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
