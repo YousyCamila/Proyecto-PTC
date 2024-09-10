ddada51f8f94156ed59136c721a5ff9a61dc4cc2
YousyCamila <166034381+YousyCamila@users.noreply.github.com>
2024-09-10 12:27:24 -0500

    feat(back-end): Arreglando problemas con la conexion de base de datos

--- 17.PTCMVC/PrivateInvestigationTechnology/Program.cs
+++ 17.PTCMVC/PrivateInvestigationTechnology/Program.cs
@@ -2,11 +2,15 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using PrivateInvestigationTechnology.Data;
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("PrivateInvestigationTechnologyContextConnection") ?? throw new InvalidOperationException("Connection string 'PrivateInvestigationTechnologyContextConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
-builder.Services.AddDbContext<PrivateInvestigationTechnologyContext>(options => options.UseSqlServer(connectionString));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<PrivateInvestigationTechnologyContext>();
+builder.Services.AddDbContext<PrivateInvestigationTechnologyContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<PrivateInvestigationTechnologyContext>();
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
