3fffe3ff885c91e2eb033ef12bcfe19cab4616d3
manu0931 <manuel25142808@gmail.com>
2024-10-24 17:12:39 -0500

    feat(front-end): Add changes in the contactanos

--- 19.PTCReact/PTC/src/pages/contactanos/contactanos.jsx
+++ 19.PTCReact/PTC/src/pages/contactanos/contactanos.jsx
@@ -6,6 +6,7 @@ import './contactanos.css';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 const Contactanos = () => {
   const [formData, setFormData] = useState({
@@ -131,7 +132,7 @@ const Contactanos = () => {
             <Button
               type="submit"
               variant="contained"
-              fullWidth 
+              fullWidth
               className="submit-button"
             >
               Enviar
@@ -140,37 +141,43 @@ const Contactanos = () => {
         </Container>
 
         <Box className="instructions-container">
-          <Typography variant="h5" gutterBottom>Instrucciones:</Typography>
-          <Typography variant="body1">
+          <Typography variant="h1" gutterBottom sx={{ fontSize: '1.7rem', fontWeight: 'bold' }}>Instrucciones:</Typography>
+          <Typography variant="body1" sx={{ fontSize: '1.3rem' }}>
             Por favor, complete el formulario con sus datos. Nos pondremos en contacto con usted lo antes posible.
           </Typography>
-          <Box sx={{ marginTop: '2rem', padding: '1.5rem', border: '1px solid #00448CFF', borderRadius: '8px', backgroundColor: '#f0f8ff', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
-            <Typography variant="h6" sx={{ fontWeight: 'bold', color: '#00448DFF' }}>Información de Contacto</Typography>
-            <Typography variant="body1" sx={{ marginTop: '0.5rem', color: '#333' }}>
-              Teléfono: <span style={{ fontWeight: 'bold' }}>350 797 2131</span>
-              <br />
-              Dirección: <span style={{ fontWeight: 'bold' }}>Carrera 15 # 79-70</span>
-              <br />
-              Ciudad: <span style={{ fontWeight: 'bold' }}>Bogotá</span>
-              <br />
-              País: <span style={{ fontWeight: 'bold' }}>Colombia</span>
-            </Typography>
+          <Box sx={{ marginTop: '3rem', padding: '3.8rem', border: '1px solid #00448CFF', borderRadius: '8px', backgroundColor: '#f0f8ff', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', marginBottom: '4rem ', display: 'flex', alignItems: 'center' }}>
+            <Box sx={{ flex: 1 }}>
+              <Typography variant="h5" sx={{ fontWeight: 'bold', color: '#00448DFF' }}>Información de Contacto</Typography>
+              <Typography variant="body1" sx={{ marginTop: '1.1rem', color: '#333', fontSize: '1.1rem' }}>
+                Teléfono: <span style={{ fontWeight: 'bold' }}>350 797 2131</span>
+                <br />
+                Dirección: <span style={{ fontWeight: 'bold' }}>Carrera 15 # 79-70</span>
+                <br />
+                Ciudad: <span style={{ fontWeight: 'bold' }}>Bogotá</span>
+                <br />
+                País: <span style={{ fontWeight: 'bold' }}>Colombia</span>
+              </Typography>
+            </Box>
+            <a href="https://www.google.com/maps?q=Carrera+15+%2379-70,Bogotá,Colombia" target="_blank" rel="noopener noreferrer" style={{ display: 'flex', alignItems: 'center', marginLeft: '1rem' }}>
+              <LocationOnIcon sx={{ fontSize: '100px', color: '#00448DFF', marginRight: '0.5rem' }} />
+              <Typography variant="body2" sx={{ color: '#333', fontWeight: 'bold' }}>Encuéntranos acá, Oficina 400</Typography> {/* Aplicar negrita al texto */}
+            </a>
           </Box>
 
           <Box sx={{ marginTop: '1rem' }}>
-    <Typography variant="body1" sx={{ marginBottom: '0.5rem' }}>Síguenos en:</Typography>
-    <Box sx={{ display: 'flex', gap: '1rem' }}>
-      <a href="https://www.facebook.com/ptcinvestigadorprivado" target="_blank" rel="noopener noreferrer">
-        <FacebookIcon />
-      </a>
-      <a href="https://www.youtube.com/@ptcinvestigationtechnology2936/videos" target="_blank" rel="noopener noreferrer">
-        <YouTubeIcon />
-      </a>
-      <a href="https://www.instagram.com/ptcinvestigationtechnology/">
-        <InstagramIcon />
-      </a>
-    </Box>
-  </Box>
+            <Typography variant="body1" sx={{ marginBottom: '0.5rem' }}>Síguenos en:</Typography>
+            <Box sx={{ display: 'flex', gap: '1rem' }}>
+              <a href="https://www.facebook.com/ptcinvestigadorprivado" target="_blank" rel="noopener noreferrer">
+                <FacebookIcon sx={{ fontSize: '1.8rem'}} />
+              </a>
+              <a href="https://www.youtube.com/@ptcinvestigationtechnology2936/videos" target="_blank" rel="noopener noreferrer">
+                <YouTubeIcon sx={{ fontSize: '1.8rem'}} />
+              </a>
+              <a href="https://www.instagram.com/ptcinvestigationtechnology/">
+                <InstagramIcon sx={{ fontSize: '1.8rem'}} />
+              </a>
+            </Box>
+          </Box>
 
         </Box>
       </Box>
