bf28aab00132d8da2f372c80f014eb8a4ad845f4
Juan Sebastian Odoñez Sierra <juanseordo2206@outlook.com>
2024-11-14 23:46:21 -0500

    Feat (Front-end, Back-end): Refactor JWT in the files middleware folder, usuarioController.js, Login.jsx and install decoded JWT

---
+++ 18.PTCNodeJS/backend/middleware/authorizeRole.js
@@ -0,0 +1,9 @@
+const authorizeRole = (requiredRole) => (req, res, next) => {
+    if (req.user.role !== requiredRole) {
+      return res.status(403).json({ message: 'Acceso denegado. Rol insuficiente' });
+    }
+    next();
+  };
+  
+  module.exports = authorizeRole;
+  
